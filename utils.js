ed5600d42ad887e15196cdeeca52da5420926b43
Vinícius Goulart <goulart.vs@gmail.com>
2016-12-04 19:49:53 -0200

    Adds function in utils.js to verify if the URL is valid

--- utils.js
+++ utils.js
@@ -7,15 +7,19 @@ const _ = require("lodash");
 
 const utils = {};
 
-utils["execute"] = function(command, environment = ""){
-    cmd.run("start " + utils.checkPrefix(command));
+utils["execute"] = function(command, environment = "") {
+    cmd.run("start " + this.checkPrefix(command));
 };
 
-utils["checkPrefix"] = function(url){
-    if(_.startsWith(url, "http://") || _.startsWith(url, "https://"))
+utils["checkPrefix"] = function(url) {
+    if(this.isValidURL(url))
         return url;
     else
         return "http://" + url;
 };
+
+utils["isValidURL"] = function(url) {
+    return _.startsWith(url, "http://") || _.startsWith(url, "https://");
+}
 
 module.exports = utils;
